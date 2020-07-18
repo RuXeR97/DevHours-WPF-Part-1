f144baebad82448186fb45b54eaa8be6d59ba951
Rafał Dębski <ruxer0097@gmail.com>
2020-07-18 13:13:31 +0200

    Final code

--- DevHours-WPF/DevHours-WPF/MainWindow.xaml.cs
+++ DevHours-WPF/DevHours-WPF/MainWindow.xaml.cs
@@ -20,15 +20,49 @@ namespace DevHours_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<ColorMatch> colors;
         public MainWindow()
         {
             InitializeComponent();
+            colors = new List<ColorMatch>()
+            {
+                new ColorMatch()
+                {
+                    Color = Brushes.Red,
+                    DecisiveCounter = 0
+                },
+                new ColorMatch()
+                {
+                    Color = Brushes.Green,
+                    DecisiveCounter = 1
+                },
+                new ColorMatch()
+                {
+                    Color = Brushes.Purple,
+                    DecisiveCounter = 2
+                }
+            };
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            testLabel.Content += "a";
-            testLabel.Background = Brushes.Aqua;
+            redGrid1.Background = redGrid2.Background = redGrid3.Background = redGrid4.Background = colors.First(i => i.DecisiveCounter == 0).Color;
+            greenGrid1.Background = greenGrid2.Background = greenGrid3.Background = greenGrid4.Background = colors.First(i => i.DecisiveCounter == 1).Color;
+            purpleGrid1.Background = colors.First(i => i.DecisiveCounter == 2).Color;
+
+            foreach(var colorMatch in colors)
+            {
+                colorMatch.DecisiveCounter++;
+                colorMatch.DecisiveCounter %= 3;
+            }
+            //testLabel.Content += "a";
+            //testLabel.Background = Brushes.Aqua;
         }
+    }
+
+    public class ColorMatch
+    {
+        public Brush Color { get; set; }
+        public int DecisiveCounter { get; set; }
     }
 }
